3f5511ee02c74eb0c58a083eb5e489ddeb0735f7
Gilson Russo <44848212+gilsonrusso@users.noreply.github.com>
2023-05-01 20:34:53 +0000

    Add radio group function

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,12 @@
-import * as Dialog from '@radix-ui/react-dialog';
-import { X } from 'phosphor-react';
-import { CloseButton, Content, Overlay } from './styles';
+import * as Dialog from "@radix-ui/react-dialog";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from "./styles";
 
 export function NewTransactionModal() {
   return (
@@ -9,17 +15,27 @@ export function NewTransactionModal() {
       <Content>
         <Dialog.Title>New Transaction</Dialog.Title>
         <CloseButton>
-          <X size={24}/>
+          <X size={24} />
         </CloseButton>
         <form action="">
-          <input type="text" placeholder='Description' required />
-          <input type="text" placeholder='Price' required />
-          <input type="text" placeholder='Category' required />
+          <input type="text" placeholder="Description" required />
+          <input type="text" placeholder="Price" required />
+          <input type="text" placeholder="Category" required />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income" value="income">
+              <ArrowCircleUp size={24} />
+                Deposit
+            </TransactionTypeButton>
+            <TransactionTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown size={24} />
+                Withdraw
+            </TransactionTypeButton>
+          </TransactionType>
 
           <button type="submit">Register</button>
         </form>
-
       </Content>
     </Dialog.Portal>
-  )
+  );
 }
