de06e926a374497d7599d8a15861acb9278bef38
Gilson Russo <gilsonrusso@outlook.com>
2023-05-01 15:33:56 -0400

    Add styles to modal component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
 import logoImg from '../../assets/Logo.svg';
+import { NewTransactionModal } from '../NewTransactionModal';
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+
+import * as Dialog from '@radix-ui/react-dialog';
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt=""/>
-        <NewTransactionButton>New transaction</NewTransactionButton>
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <NewTransactionButton>New transaction</NewTransactionButton>
+            </Dialog.Trigger>
+            <NewTransactionModal/>
+          </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
